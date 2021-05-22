9ada2c0735491fef08405fce981ab895d3d0e4be
AlfreeanaAlfie <61505268+alfreeana-alfie@users.noreply.github.com>
2021-05-23 01:05:05 +0800

    update

--- planner/lib/auth/forgot_password.dart
+++ planner/lib/auth/forgot_password.dart
@@ -1,55 +1,96 @@
+import 'dart:convert';
+import 'package:MyUni/auth/sign_in.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:oktoast/oktoast.dart';
 
+import 'package:MyUni/models/Verify.dart';
+
 class ForgotPassword extends StatefulWidget {
+  String matric_no;
+  String email;
+
+  ForgotPassword({Key key, this.matric_no, this.email}) : super(key: key);
+
   @override
   _ForgotPasswordState createState() => _ForgotPasswordState();
 }
 
 class _ForgotPasswordState extends State<ForgotPassword> {
+
+  // Variables
   String password = "";
   String password_confirm = "";
   String userID;
 
   final _formKey = GlobalKey<FormState>();
+  Map<String, dynamic> verifyMap;
 
   // Methods
   void getData() async {
-    String loginString = "http://hawkingnight.com/my-uni/public/api/forgot";
-    Uri loginURI = Uri.parse(loginString);
+    Uri loginURI =
+        Uri.parse("https://hawkingnight.com/planner/public/api/forgot");
     await http.post(loginURI, headers: {
       "Accept": "application/json"
     }, body: {
-      "id": userID,
+      "email": widget.email,
+      "matric_no": widget.matric_no,
       "password": password,
     }).then((value) => print("Response Status : ${value.statusCode}"));
+
+    final response = await http
+        .post(
+          loginURI, 
+          headers: {"Accept": "application/json"}, 
+          body: {
+            "email": widget.email,
+            "matric_no": widget.matric_no,
+            "password": password
+          });
+
+    if(response.statusCode == 200) {
+      verifyMap = jsonDecode(response.body);
+
+      var verifyData = Verify.fromJSON(verifyMap);
+      if (verifyData.status == "Success") {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text("Reset Successfully")));
+            
+        Navigator.push(
+          context, MaterialPageRoute(builder: (context) => Login()));
+      }else{
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text("Incorrect Password/Confirm Password")));
+      }
+    }else{
+      throw Exception('Unable to fetch data from the REST API');
+    }
   }
 
   // Widgets
   Widget form() {
     return Form(
-            key: _formKey,
-            child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  icon: Icon(Icons.lock),
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    password = value;
-                  });
-                },
-                validator: (val) {
-                  if (val.isEmpty) {
-                    return 'Password is empty!';
-                  }
-                  return null;
-                },
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+                icon: Icon(Icons.lock),
               ),
-              TextFormField(
+              onChanged: (value) {
+                setState(() {
+                  password = value;
+                });
+              },
+              validator: (val) {
+                if (val.isEmpty) {
+                  return 'Password is empty!';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
                 decoration: InputDecoration(
                   labelText: 'Confirm Password',
                   icon: Icon(Icons.lock),
@@ -61,44 +102,43 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                 },
                 obscureText: true,
                 validator: (val) {
-                  if(val.isEmpty){
+                  if (val.isEmpty) {
                     return 'Confirm Password is empty!';
-                  }else if (val == password){
-                    return 'Confirm Password does not match!'; 
+                  } else if (val == password) {
+                    return 'Confirm Password does not match!';
                   }
                   return null;
+                }),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  getData();
+                } else {
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text("Error!")));
                 }
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    getData();
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Error!")));
-                  }
-                },
-                child: Text('RESET NEW PASSWORD',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    )),
-              ),
-            ],
-          ));
+              },
+              child: Text('RESET NEW PASSWORD',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+          ],
+        ));
   }
 
   @override
   Widget build(BuildContext context) {
     return OKToast(
-      child: Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
-              backgroundColor: Colors.red[900],
-              elevation: 0.0,
-              title: Center(
-                child: Text('FORGOT PASSWORD?'),
-              )),
-          body: form(),
-        )
-    );
+        child: Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+          backgroundColor: Colors.red[900],
+          elevation: 0.0,
+          title: Center(
+            child: Text('FORGOT PASSWORD?'),
+          )),
+      body: form(),
+    ));
   }
 }
--- planner/lib/auth/verify_email.dart
+++ planner/lib/auth/verify_email.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:http/http.dart' as http;
+
+import 'package:MyUni/models/Verify.dart';
+
+import 'forgot_password.dart';
 
 class Verification extends StatefulWidget {
   @override
@@ -8,47 +13,63 @@ class Verification extends StatefulWidget {
 }
 
 class _VerificationState extends State<Verification> {
-  String matricNo = "";
-  String email = "";
-
+  // Variables
   final _formKey = GlobalKey<FormState>();
 
-  // Methods 
+  String matricNo = "";
+  String email = "";
+  Map<String, dynamic> verifyMap;
+
+  // Methods
   void verify() async {
-    String loginString = "http://hawkingnight.com/my-uni/public/api/verify";
-    Uri loginURI = Uri.parse(loginString);
-    await http.post(loginURI, headers: {
-      "Accept": "application/json"
-    }, body: {
-      "matric_no": matricNo,
-      "email": email,
-    }).then((value) => print("Response Status : ${value.statusCode}"));
+    Uri getVerifyURL =
+        Uri.parse("https://hawkingnight.com/planner/public/api/verify");
+    final response = await http.post(getVerifyURL,
+        headers: {"Accept": "application/json"},
+        body: {"matric_no": matricNo, "email": email});
+
+    if (response.statusCode == 200) {
+      print('Succcess Verified');
+      verifyMap = jsonDecode(response.body);
+
+      var verifyData = Verify.fromJSON(verifyMap);
+
+      if (verifyData.status == "Success") {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => ForgotPassword(
+              matric_no: matricNo,
+              email: email
+            )));
+      }
+    } else {
+      throw Exception('Unable to fetch data from the REST API');
+    }
   }
 
   // Widgets
   Widget form() {
     return Form(
-            key: _formKey,
-            child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Matric No',
-                  icon: Icon(Icons.person),
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    matricNo = value;
-                  });
-                },
-                validator: (val) {
-                  if (val.isEmpty) {
-                    return 'Matric No is empty!';
-                  }
-                  return null;
-                },
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Matric No',
+                icon: Icon(Icons.person),
               ),
-              TextFormField(
+              onChanged: (value) {
+                setState(() {
+                  matricNo = value;
+                });
+              },
+              validator: (val) {
+                if (val.isEmpty) {
+                  return 'Matric No is empty!';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
                 decoration: InputDecoration(
                   labelText: 'Email Address',
                   icon: Icon(Icons.lock),
@@ -60,42 +81,40 @@ class _VerificationState extends State<Verification> {
                 },
                 obscureText: true,
                 validator: (val) {
-                  if(val.isEmpty){
+                  if (val.isEmpty) {
                     return 'Email Address is empty!';
                   }
                   return null;
+                }),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  verify();
+                } else {
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text("Error!")));
                 }
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    verify();
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Error!")));
-                  }
-                },
-                child: Text('VERIFY',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    )),
-              ),
-            ],
-          ));
+              },
+              child: Text('VERIFY',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+          ],
+        ));
   }
 
   @override
   Widget build(BuildContext context) {
     return OKToast(
-      child: Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
-              backgroundColor: Colors.red[900],
-              elevation: 0.0,
-              title: Center(
-                child: Text('VERIFICATION'),
-              )),
-          body: form()
-        )
-    );
+        child: Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+                backgroundColor: Colors.red[900],
+                elevation: 0.0,
+                title: Center(
+                  child: Text('VERIFICATION'),
+                )),
+            body: form()));
   }
 }
